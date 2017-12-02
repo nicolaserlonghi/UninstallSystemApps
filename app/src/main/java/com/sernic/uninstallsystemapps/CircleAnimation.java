484da24e4523d4433f62d86f2380289394e0f56b
Sernic <31650038+Sernic@users.noreply.github.com>
2017-12-02 15:39:15 +0100

    Add circle progres animation

---
+++ app/src/main/java/com/sernic/uninstallsystemapps/CircleAnimation.java
@@ -0,0 +1,74 @@
+package com.sernic.uninstallsystemapps;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.ConditionVariable;
+import android.os.Handler;
+
+import at.grabner.circleprogress.CircleProgressView;
+import at.grabner.circleprogress.TextMode;
+
+/**
+ * Created by nicola on 02/12/17.
+ */
+
+public class CircleAnimation extends AsyncTask<Boolean, Void, Boolean> {
+
+    private final Handler handler;
+    private CircleProgressView mCircleView;
+
+    public ConditionVariable conditionVariable = new ConditionVariable();
+    public CircleAnimation(Context context, CircleProgressView mCircleView) {
+        handler = new Handler(context.getMainLooper());
+        this.mCircleView = mCircleView;
+    }
+
+    @Override
+    protected Boolean doInBackground(final Boolean... status) {
+
+
+
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                mCircleView.setSeekModeEnabled(false);
+                mCircleView.setSpinningBarLength(80);
+                mCircleView.setSpinSpeed(2);
+                mCircleView.setShowTextWhileSpinning(true);
+
+                //Status = true loading animation
+                //Stautus = false execute animation
+                if(status[0]) {
+                    mCircleView.setText("Loading...");
+                    mCircleView.setTextMode(TextMode.TEXT);
+                    mCircleView.setUnitVisible(false);
+                    mCircleView.spin();
+               } else {
+                    mCircleView.setValue(0);
+               }
+            }
+        });
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return status[0];
+    }
+
+    @Override
+    protected void onPostExecute(Boolean status) {
+
+
+        if(status)
+            mCircleView.stopSpinning();
+        else
+            mCircleView.setValueAnimated(100);
+    }
+}
+
--- app/src/main/java/com/sernic/uninstallsystemapps/MainActivity.java
+++ app/src/main/java/com/sernic/uninstallsystemapps/MainActivity.java
@@ -1,13 +1,27 @@
 package com.sernic.uninstallsystemapps;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import at.grabner.circleprogress.CircleProgressView;
+
+public class MainActivity extends FragmentActivity {
+
+    private CircleAnimation circleAnimation;
+    private CircleProgressView circleProgressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        circleProgressView = (CircleProgressView)findViewById(R.id.circleView);
+
+        // Start initial animation of load
+        CircleAnimation circleAnimation = new CircleAnimation(this, circleProgressView);
+        circleAnimation.execute(true);
+
+
     }
 }
