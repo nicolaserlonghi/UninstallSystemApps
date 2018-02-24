b36732bf9ca3384ca3494c0fa6c43dc3be2ac4f2
Sernic <31650038+Sernic@users.noreply.github.com>
2018-02-24 09:07:16 +0100

    Add Splash Screen, fix select all button

---
+++ app/src/main/java/com/sernic/uninstallsystemapps/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.sernic.uninstallsystemapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
